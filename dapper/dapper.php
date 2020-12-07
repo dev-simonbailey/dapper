73c7dac3b74ce2b5cc419b2d697e34f5ac53b488
Simon Bailey <webdisk@hotmail.com>
2020-12-07 10:49:30 +0000

    added feedback page

--- dapper/dapper.php
+++ dapper/dapper.php
@@ -19,6 +19,10 @@ $encoding = 'latin1';
 
 
 // NOTE: Nothing needs modifying below this line, but feel free to adapt it to your needs.
+$displayFile = fopen(__DIR__."/display/index.php","w");
+$displayString = '<?php'."\n";
+$displayString.= 'require(__DIR__."/autoloader.php");'."\n";
+fwrite($displayFile,$displayString);
 require(__DIR__."/classes/dapper.DB.class.php");
 $DB = new MeekroDB($host, $user, $password, $dbName, $port, $encoding);
 echo "MAPPING STARTED\n";
@@ -26,13 +30,13 @@ $db_tables = $DB->tableList();
 foreach ($db_tables as $table) {
   echo "PROCESSING TABLE ==> ".$table."\n";
   $columns = $DB->columnList($table);
-  if(!file_exists(__DIR__."/../classes/tables/".$table."/".$table.".php")){
-    mkdir(__DIR__."/../classes/tables/".$table, 0777, true);
+  if(!file_exists(__DIR__."/../classes/Tables/".$table."/".$table.".php")){
+    mkdir(__DIR__."/../classes/Tables/".$table, 0777, true);
   }
-  $file = fopen(__DIR__."/../classes/tables/".$table."/".$table.".php","w");
+  $classFile = fopen(__DIR__."/../classes/Tables/".$table."/".$table.".php","w");
   $classString = '<?php'."\n";
   $classString.= "\t".'declare(strict_types = 1);'."\n\n";
-  $classString.= "\t".'namespace Tables\\'.ucwords($table).';'."\n\n";
+  $classString.= "\t".'namespace Tables\\'.$table.';'."\n\n";
   $classString.= "\t".'class '.$table.' implements '.$table.'Interface {'."\n\n";
   $classString.= "\t\t".'private $table = "'.$table.'";'."\n\n";
   $columns_array = '';
@@ -83,18 +87,27 @@ foreach ($db_tables as $table) {
   $classString.= "\t\t\t\t".'}'."\n";
   $classString.= "\t\t\t".'}'."\n";
   $classString.= "\t\t".'}'."\n";
-  fwrite($file,$classString);
-  fclose($file);
-  $file = fopen(__DIR__."/../classes/tables/".$table."/".$table."Interface.php","w");
+  fwrite($classFile,$classString);
+  fclose($classFile);
+  $interfaceFile = fopen(__DIR__."/../classes/Tables/".$table."/".$table."Interface.php","w");
   $interfaceString = '<?php'."\n\n";
   $interfaceString.= "\t\t".'declare(strict_types = 1);'."\n\n";
-  $interfaceString.= "\t\t".'namespace Tables\\'.ucwords($table).';'."\n\n";
+  $interfaceString.= "\t\t".'namespace Tables\\'.$table.';'."\n\n";
   $interfaceString.= "\t\t".'interface '.$table.'Interface {'."\n\n";
   $interfaceString.= "\t\t\t".'public function getColumns(): array;'."\n\n";
   $interfaceString.= "\t\t\t".'public function getSchema(): array;'."\n\n";
   $interfaceString.= "\t\t\t".'public function checkColumns(array $cols): string;'."\n\n";
   $interfaceString.= "\t\t".'}'."\n\n";
-  fwrite($file,$interfaceString);
-  fclose($file);
+  fwrite($interfaceFile,$interfaceString);
+  fclose($interfaceFile);
+  $displayString = '$'.$table.' = new Tables\\'.$table.'\\'.$table.';'."\n";
+  $displayString.= 'echo "'.$table.' ==> COLUMN NAMES: <br />";'."\n";
+  $displayString.= 'var_dump($'.$table.'->getColumns());'."\n";
+  $displayString.= "echo '<br /><br />';\n";
+  $displayString.= 'echo "'.$table.' ==> COLUMN SCHEMA: <br />";'."\n";
+  $displayString.= 'var_dump($'.$table.'->getSchema());'."\n";
+  $displayString.= 'echo "<br /><br />";'."\n";
+  fwrite($displayFile,$displayString);
 }
+fclose($displayFile);
 echo "MAPPING COMPLETE";
